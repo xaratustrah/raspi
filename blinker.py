633d3973ae45a4bb5a06d667cd4c9a3db40a6d77
Xaratustrah <shahab.sanjari@gmail.com>
2016-02-12 16:49:00 +0100

    bump

--- blinker.py
+++ blinker.py
@@ -10,20 +10,19 @@ Xaratustrah
 import time
 import RPi.GPIO as gpio
 
-gpio.setmode(gpio.BOARD)
-
 LED = 31  # assign pin number
 
-gpio.setup(LED, gpio.OUT)
 gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(LED, gpio.OUT)
 
 print('Blinker started. ctrl-c to abort.\n')
 try:
     while True:
         gpio.output(31, gpio.HIGH)
-        time.sleep(0.5)
+        time.sleep(0.3)
         gpio.output(31, gpio.LOW)
-        time.sleep(0.5)
+        time.sleep(0.3)
 
 except(EOFError, KeyboardInterrupt):
     print('\nUser input cancelled. Aborting...')
